b3b69f55ce7da3de423f95237ce0dda52730590c
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-03-15 17:00:16 +0100

    updated metas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Memora AI - Preserving memories and connecting generations. Powered by Affinidi and ConvexDev. Explore the future of remembrance today!" />
+        <meta property="og:title" content="Memora AI" />
+        <meta property="og:description" content="Preserving memories and connecting generations. Powered by Affinidi and ConvexDev. Explore the future of remembrance today!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://memora-ai.vercel.app/" />
+        <meta property="og:image" content="https://i.postimg.cc/mkZGzCtg/ai-hero.png" />
+        <meta name="twitter:title" content="Memora AI" />
+        <meta name="twitter:description" content="Preserving memories and connecting generations. Powered by Affinidi and ConvexDev. Explore the future of remembrance today!" />
+        <meta name="twitter:image" content="https://i.postimg.cc/mkZGzCtg/ai-hero.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <title>
-        Memora
+        Memora AI
       </title>
       <body>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
